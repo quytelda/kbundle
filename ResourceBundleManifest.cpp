41765367c7bce704f896854314e9c5b12150e346
Quytelda Kahja <quytelda@tamalin.org>
2023-06-21 02:05:25 -0700

    Add ResourceBundleManifest class.

---
+++ ResourceBundleManifest.cpp
@@ -0,0 +1,80 @@
+#include "ResourceBundleManifest.hpp"
+
+ResourceBundleManifest::ResourceBundleManifest(const QDir &root)
+{
+    QString manifestPath = root.absoluteFilePath(MANIFEST_PATH);
+    manifestFile = new QFile(manifestPath);
+}
+
+ResourceBundleManifest::~ResourceBundleManifest()
+{
+    delete manifestFile;
+}
+
+bool ResourceBundleManifest::exists()
+{
+    return manifestFile->exists();
+}
+
+bool ResourceBundleManifest::load()
+{
+    if (!manifestFile->open(QIODevice::ReadOnly)) {
+        return false;
+    }
+
+    if (!doc.setContent(manifestFile, true)) {
+        manifestFile->close();
+        return false;
+    }
+    manifestFile->close();
+
+    return true;
+}
+
+bool ResourceBundleManifest::create()
+{
+    return false;
+}
+
+bool ResourceBundleManifest::findFileEntry(const QString &full_path, FileEntry *entry)
+{
+    QDomElement root = doc.documentElement();
+    if(root.isNull()) {
+        return false;
+    }
+
+    QDomElement e;
+    for (e = root.firstChildElement(); !e.isNull(); e = e.nextSiblingElement()) {
+        if (!parseFileEntry(e, entry)) {
+            std::cerr << "Failed to parse file entry on line "
+                      << e.lineNumber()
+                      << std::endl;
+            continue;
+        }
+
+        if (entry->full_path == full_path) {
+            return true;
+        }
+    }
+
+    return false;
+}
+
+bool ResourceBundleManifest::addFileEntry(const FileEntry &entry)
+{
+    QDomElement root = doc.documentElement();
+    if (root.isNull()) {
+        return false;
+    }
+
+    QDomElement elem = doc.createElementNS(MANIFEST_XMLNS, TAG_FILE_ENTRY);
+    elem.setAttributeNS(MANIFEST_XMLNS, ATTR_MEDIA_TYPE, entry.media_type);
+    elem.setAttributeNS(MANIFEST_XMLNS, ATTR_FULL_PATH , entry.full_path);
+    elem.setAttributeNS(MANIFEST_XMLNS, ATTR_MD5SUM    , entry.md5sum);
+
+    // TODO: Use QDomNode::insertAfter(newChild, refChild) to keep
+    // matching media-types together.
+    root.appendChild(elem);
+
+    return true;
+}
---
+++ ResourceBundleManifest.hpp
@@ -0,0 +1,54 @@
+#ifndef __RESOURCE_BUNDLE_MANIFEST_HPP
+#define __RESOURCE_BUNDLE_MANIFEST_HPP
+
+#include <iostream>
+
+#include <QtCore>
+#include <QtXml>
+
+#define MANIFEST_PATH "META-INF/manifest.xml"
+#define MANIFEST_XMLNS "urn:oasis:names:tc:opendocument:xmlns:manifest:1.0"
+
+#define TAG_FILE_ENTRY  "file-entry"
+#define ATTR_MEDIA_TYPE "media-type"
+#define ATTR_FULL_PATH  "full-path"
+#define ATTR_MD5SUM     "md5sum"
+
+struct FileEntry {
+    QString full_path;
+    QString media_type;
+    QString md5sum;
+};
+
+bool parseFileEntry(const QDomElement &elem, FileEntry *entry)
+{
+    if (elem.tagName() != TAG_FILE_ENTRY) {
+        return false;
+    }
+
+    entry->media_type = elem.attributeNS(MANIFEST_XMLNS, ATTR_MEDIA_TYPE);
+    entry->full_path  = elem.attributeNS(MANIFEST_XMLNS, ATTR_FULL_PATH);
+    entry->md5sum     = elem.attributeNS(MANIFEST_XMLNS, ATTR_MD5SUM);
+
+    return true;
+}
+
+class ResourceBundleManifest
+{
+public:
+    ResourceBundleManifest(const QDir &root);
+    ~ResourceBundleManifest();
+
+    bool exists();
+    bool load();
+    bool create();
+    bool findFileEntry(const QString &full_path, FileEntry *entry);
+    bool addFileEntry(const FileEntry &entry);
+    bool save();
+
+private:
+    QFile *manifestFile;
+    QDomDocument doc;
+};
+
+#endif /* __RESOURCE_BUNDLE_MANIFEST_HPP */

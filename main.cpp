c7221527b1f5b3d5d1fba0aad1104fe5ccc78848
Quytelda Kahja <quytelda@tamalin.org>
2023-06-24 21:00:14 -0700

    Add more advanced argument parsing.

--- main.cpp
+++ main.cpp
@@ -2,17 +2,36 @@
 
 #include "ResourceBundle.hpp"
 
+#define REQUIRE_ARGS(n, args)                              \
+    if ((args).size() < (n)) {                             \
+        std::cerr << "Not enough arguments." << std::endl; \
+        return 1;                                          \
+    }
+
+
 int main(int argc, char** argv)
 {
-    if (argc < 3) {
+    QString path = QDir::currentPath();
+
+    // Parse the argument list.
+    QStringList args;
+    for (int i = 1; i < argc; i++) {
+        QString arg(argv[i]);
+        if (arg == "-r" && (i+1) < argc) {
+            path = QString(argv[++i]);
+            continue;
+        }
+
+        args.append(arg);
+    }
+
+    if (args.isEmpty()) {
         std::cerr << "Not enough arguments!\n"
                   << "Usage: kbundle <COMMAND> [ARGS...] <PATH>"
                   << std::endl;
         return 1;
     }
-
-    QString command(argv[1]);
-    QString path(argv[argc - 1]);
+    QString command = args.takeFirst();
 
     ResourceBundle bundle(path);
     if (!bundle.init()) {
@@ -26,9 +45,11 @@ int main(int argc, char** argv)
     if (command == "update") {
         ok = bundle.updateManifest();
     } else if (command == "add-tag") {
-        ok = bundle.addTag(argv[2], argv[3]);
+        REQUIRE_ARGS(2, args);
+        ok = bundle.addTag(args[1], args[0]);
     } else if (command == "remove-tag") {
-        ok = bundle.removeTag(argv[2], argv[3]);
+        REQUIRE_ARGS(2, args);
+        ok = bundle.removeTag(args[1], args[0]);
     } else {
         ok = false;
         std::cerr << "Unknown command: "

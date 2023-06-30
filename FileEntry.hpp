a286a21b523dc555c3e23060dd048350ffd24ed8
Quytelda Kahja <quytelda@tamalin.org>
2023-06-29 20:34:52 -0700

    Removed unused operator==() and qHash() implementations.

--- FileEntry.hpp
+++ FileEntry.hpp
@@ -28,16 +28,6 @@ struct FileEntry {
     QStringList tags;
 };
 
-inline bool operator==(const FileEntry &lhs, const FileEntry &rhs)
-{
-    return (lhs.path == rhs.path);
-}
-
-inline uint qHash(const FileEntry &key, uint seed)
-{
-    return qHash(key.path, seed);
-}
-
 inline const char *showFileEntry(const FileEntry &entry)
 {
     return qPrintable(entry.path      + ":" +
